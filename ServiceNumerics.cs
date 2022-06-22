772a8bc9e34286ea79e27ae1ecb6bda9dffe3237
todayisntmonday <tom.pop16@seznam.cz>
2022-06-22 22:52:19 +0200

    optional numerics structure to set fee system, minimum deposit

---
+++ ServiceNumerics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmicTrading {
+    internal struct ServiceNumerics {
+        public double TradingFee {get; init;}
+        public double WithdrawalFee { get; init; }
+        public int MinimumDeposit { get; init; }
+        public ServiceNumerics(double inTradingFee, double inWithdrawalFee, int inMinDeposit) {
+            TradingFee = inTradingFee;
+            WithdrawalFee = inWithdrawalFee;
+            MinimumDeposit = inMinDeposit;
+        }
+    }
+}
