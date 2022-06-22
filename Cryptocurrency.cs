e5613db0c348e6028ed523568bb4835a581d3277
todayisntmonday <tom.pop16@seznam.cz>
2022-06-22 22:49:49 +0200

    parametrized ctor added

--- Cryptocurrency.cs
+++ Cryptocurrency.cs
@@ -1,6 +1,10 @@
 enum State { Default, Buy, Sell, Hold };
 
 struct Cryptocurrency {
-    public State Action {get; set;}
-    public State Price { get; set; }
+    public State Action { get; set;}
+    public double Price { get; set; }
+    public Cryptocurrency(State inAction, double inPrice) {
+        Action = inAction;
+        Price = inPrice;
+    }
 }
