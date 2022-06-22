70e16f59ad6bc2f43cde7fc24d5540e3cd57126e
todayisntmonday <tom.pop16@seznam.cz>
2022-06-23 01:07:02 +0200

    deposit fee introduced

--- ServiceNumerics.cs
+++ ServiceNumerics.cs
@@ -7,10 +7,14 @@ using System.Threading.Tasks;
 namespace AlgorithmicTrading {
     internal struct ServiceNumerics {
         public double TradingFee {get; init;}
+        public double DepositFee { get; init; }
         public double WithdrawalFee { get; init; }
         public int MinimumDeposit { get; init; }
-        public ServiceNumerics(double inTradingFee, double inWithdrawalFee, int inMinDeposit) {
+        public ServiceNumerics(
+            double inTradingFee, double inDepositFee, 
+            double inWithdrawalFee, int inMinDeposit) {
             TradingFee = inTradingFee;
+            DepositFee = inDepositFee;
             WithdrawalFee = inWithdrawalFee;
             MinimumDeposit = inMinDeposit;
         }
