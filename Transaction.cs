a5356f5bfbc41c0ecf23634acda1965d56de02ae
todayisntmonday <tom.pop16@seznam.cz>
2022-06-26 18:15:10 +0200

    converted action change

---
+++ Transaction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmicTrading {
+    internal struct Transaction {
+        private readonly DateTime timestamp;
+        private readonly string symbol;
+        private readonly double amount;
+        private readonly double exchangeRate;
+        private readonly string action;
+        public Transaction(string inSymbol, double inAmount, double inExchangeRate, string inAction) {
+            timestamp = DateTime.Now;
+            symbol = inSymbol;
+            amount = inAmount;
+            exchangeRate = inExchangeRate;
+            action = inAction;
+        }
+
+        public override string ToString() {
+            return string.Format(
+                "{0} Symbol: {1} Price: {2:0.#####} Amount: {3:0.#####} Action: {4}",
+                timestamp, symbol, amount, exchangeRate, action
+            );
+        }
+        
+        public string GetCSVLine() {
+            return string.Format(
+                "{0},{1},{2:0.#####},{3:0.#####},{4}", 
+                timestamp, symbol, amount, exchangeRate, action
+            );
+        }
+    }
+}
