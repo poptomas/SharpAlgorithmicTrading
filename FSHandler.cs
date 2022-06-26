4a73c6f8125752392f7630bc3a3719f62cb091f7
todayisntmonday <tom.pop16@seznam.cz>
2022-06-26 18:14:31 +0200

    filesystem handler for the output csv file added

---
+++ FSHandler.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmicTrading {
+    internal class FSHandler {
+        private readonly string directory;
+        private readonly string file;
+        private TextWriter writer;
+
+        public FSHandler(string path) {
+            directory = Path.GetDirectoryName(path);
+            file = Path.GetFileName(path);
+        }
+
+        public void TryFlushPreviousRun() {
+            try {
+                if(Directory.Exists(directory)) {
+                    Directory.CreateDirectory(directory);
+                }
+                else if(File.Exists(file)) {
+                    File.Delete(file);
+                }
+            }
+            catch (Exception e){
+                Console.WriteLine(e.Message);
+            }
+            writer = new StreamWriter(file);
+        }
+
+        public void Save(string row) {
+            try {
+                writer.WriteLine(row);
+            }
+            catch(IOException exc) {
+                Console.WriteLine(exc.Message);
+            }
+        }
+    }
+}
